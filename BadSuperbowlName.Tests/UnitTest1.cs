592d43f3d06a95ed6bf874beaf1003d9aafca3bc
Martin Sisk <Brothermj@hotmail.com>
2021-10-04 13:53:33 -0500

    intial commit for BadSuperbowlName Repo.

---
+++ BadSuperbowlName.Tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+using System;
+using Xunit;
+
+namespace BadSuperbowlName.Tests
+{
+  // Test classes must be public
+  public class UnitTest1
+  {
+    // Need to have a [Fact] attribute, and needs to be public
+    [Fact]
+    public void Test1()
+    {
+      // Arrange -> context that the test is run inside of
+      var numberToTranslate = 1;
+      var expectedResult = "I";
+      var translator = new NumberTranslator();
+
+      // Act -> the actual thing we're testing, do the thing
+      var actualResult = translator.Translate(numberToTranslate);
+
+      // Assert -> lay out the expectations in such a way that, if not met,
+      // exceptions are thrown.
+
+
+      Assert.True(actualResult == expectedResult);
+      // Same result different Assert command with more detail
+      // Assert.Equal(expectedResult, actualResult);
+
+
+
+      // Tests pass if no exceptions are thrown while the test is running
+      // Tests fail if any exceptions are thrown while the test is running
+      // throw new Exception("Throwing an exception makes tests fail");
+
+    }
+    [Fact]
+    public void the_number_3_should_be_translated_toII()
+    {
+      // Arrange
+      var numberToTranslate = 3;
+      var expectedResult = "III";
+      var translator = new NumberTranslator();
+
+      // Act
+      var actualResult = translator.Translate(numberToTranslate);
+
+      // Assert
+      Assert.Equal(expectedResult, actualResult);
+
+    }
+
+    [Fact]
+    public void the_number_4_should_be_translated_toIV()
+    {
+      // Arrange
+      var numberToTranslate = 4;
+      var expectedResult = "IV";
+      var translator = new NumberTranslator();
+
+      // Act
+      var actualResult = translator.Translate(numberToTranslate);
+
+      // Assert
+      Assert.Equal(expectedResult, actualResult);
+
+    }
+  }
+}
---
+++ BadSuperbowlName/NumberTranslator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadSuperbowlName
+{
+  public class NumberTranslator
+  {
+    public string Translate(int numberToTranslate)
+    {
+      /*
+      // Switch Expression
+      return numberToTranslate switch
+      {
+        1 => "I",
+        2 => "II",
+        3 => "III",
+        4 => "IV",
+        // _ discard
+        _ => ""
+      };
+*/
+      switch (numberToTranslate)
+      {
+        case 1:
+          return "I";
+        case 2:
+          return "III";
+        case 3:
+          return "IV";
+        default:
+          return "";
+      }
+
+    }
+
+  }
+}
